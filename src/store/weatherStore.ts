51c52699c9a2bb5fa2591c6de22ffeb5e14fc4b2
lidiia-pecode <lidiia_tsymborovych@pecodesoftware.com>
2026-01-05 13:02:26 +0200

    feat(weather-store): persist locationHistory via sessionStorage to improve consistency and maintainability

--- src/store/weatherStore.ts
+++ src/store/weatherStore.ts
@@ -1,6 +1,7 @@
 'use client';
 
 import { Country, LocationData, TMetricTab, WeatherMetric } from '@/types/Weather';
+import { createJSONStorage, persist } from 'zustand/middleware';
 import { create } from 'zustand';
 
 interface WeatherState {
@@ -18,48 +19,36 @@ interface WeatherState {
   setCountry: (val: Country | null) => void;
 }
 
-export const useWeatherStore = create<WeatherState>(set => {
-  let initialHistory: LocationData[] = [];
-  if (typeof window !== 'undefined') {
-    initialHistory = JSON.parse(
-      sessionStorage.getItem('location_history') || '[]'
-    );
-  }
+export const useWeatherStore = create<WeatherState>()(
+  persist(
+    (set) => ({
+      metrics: [],
+      metricMode: 'hourly',
+      location: null,
+      locationHistory: [],
+      selectedCountry: null,
 
-  return {
-    metrics: [],
-    metricMode: 'hourly',
-    location: null,
-    locationHistory: initialHistory,
-    selectedCountry: null,
-
-    setMetrics: metrics => set({ metrics }),
-    setMetricMode: mode => set({ metricMode: mode }),
-    setLocation: location => set({ location }),
-    addLocationToHistory: loc =>
-      set(state => {
-        const filtered = state.locationHistory.filter(
-          l => l.lat !== loc.lat || l.lon !== loc.lon
-        );
-        const newHistory = [...filtered, loc];
-
-        if (typeof window !== 'undefined') {
-          sessionStorage.setItem(
-            'location_history',
-            JSON.stringify(newHistory)
+      setMetrics: metrics => set({ metrics }),
+      setMetricMode: mode => set({ metricMode: mode }),
+      setLocation: location => set({ location }),
+      addLocationToHistory: loc =>
+        set(state => {
+          const filtered = state.locationHistory.filter(
+            l => l.lat !== loc.lat || l.lon !== loc.lon
           );
-        }
+          const newHistory = [...filtered, loc];
 
-        return { locationHistory: newHistory };
-      }),
+          return { locationHistory: newHistory };
+        }),
 
-    clearHistory: () => {
-      if (typeof window !== 'undefined') {
-        sessionStorage.removeItem('location_history');
-      }
-      set({ locationHistory: [] });
-    },
+      clearHistory: () => set({ locationHistory: [] }),
 
-    setCountry: selectedCountry => set({ selectedCountry }),
-  };
-});
+      setCountry: selectedCountry => set({ selectedCountry }),
+    }),
+    {
+      name: 'weather-store',
+      storage: createJSONStorage(() => sessionStorage),
+      partialize: (state) => ({ locationHistory: state.locationHistory }),
+    }
+  )
+);
